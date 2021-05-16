a15f996e2b85e64cc8bc1ee9d9ea629c20f668c4
Ayan Khan <beaayank@gmail.com>
2021-05-17 02:19:53 +0530

    check

--- test.php
+++ test.php
@@ -1,10 +1,23 @@
 <?php 
 
 
+    include 'connection.php';
+    $conn = mysqli_connect($host, $user, $password, $db);
+    if (!$conn) {
+        echo "connection failed";
+    }
 
-include 'connection.php';
-$conn = OpenCon();
-echo "Connected Successfully";
-CloseCon($conn);
+    if (isset($_REQUEST["new_post"])) {
+        $title = $_REQUEST["title"];
+        $content = $_REQUEST["blog"];
 
- ?>
+        $sql = "INSERT INTO data(title, blog) VALUES('$title', '$content')";
+        mysqli_query($conn, $sql);
+
+        echo $sql;
+        exit();
+    }
+
+    
+
+?>
